5086bf542398e433f59c9b2ee182c3c4495aa41c
wwnss00 <wns208500@gmail.com>
2025-04-28 22:31:18 +0900

    IoC/DI

---
+++ oop_study1/src/chap03/encapsulation01/ClassA.java
@@ -0,0 +1,16 @@
+package chap03.encapsulation01;
+
+public class ClassA {
+    private int pri;
+    int def;
+    protected int pro;
+    public int pub;
+
+    void runSomething() {
+
+    }
+
+    static void runStaticThing() {
+
+    }
+}
